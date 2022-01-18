acd5a45ff595c83e62a97a3bc9c90abb7462ef26
vrundan28 <vrundan1234@gmail.com>
2022-01-18 17:15:49 +0530

    Upload a Movie-Backend

--- MovieRecommendationApp/MovieOperations/views.py
+++ MovieRecommendationApp/MovieOperations/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.core.files.storage import default_storage
+from django.http.response import JsonResponse
+from rest_framework.parsers import JSONParser
+
 
 # Create your views here.
+
+@csrf_exempt
+def UploadMovie(request):
+    if request.method == "POST":
+        movie = request.FILES["MovieVideo"]
+        movieName = request.POST["movieName"]
+        res = default_storage.save(movieName,movie)
+        return JsonResponse("res",safe=False)
+
+
