040194590ef5c7d5b59ee8964eccda45d5e3d8c3
Harprit <yadavharpritsinh@gmail.com>
2022-01-31 22:03:55 +0530

    Return recommended movies in proper form

--- MovieRecommendationApp/MovieOperations/models.py
+++ MovieRecommendationApp/MovieOperations/models.py
@@ -1,3 +1,4 @@
+from re import M
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 from Accounts.models import CustomUser
@@ -21,6 +22,24 @@ class Movie(models.Model):
     ])
     movieUrl = models.CharField(max_length=1024)
     moviePoster = models.CharField(max_length=1024, null=True)
+
+    # def __init__(self,movieId,movieTitle,movieDescription,movieKeywords,movieCast,movieDirector,movieProduction,movieRuntime,movieGenre,movieTagline,movieRating,movieUrl,moviePoster):
+    #     self.movieId=movieId
+    #     self.movieTitle=movieTitle
+    #     self.movieDescription=movieDescription
+    #     self.movieKeywords=movieKeywords
+    #     self.movieCast=movieCast
+    #     self.movieDirector=movieDirector
+    #     self.movieProduction=movieProduction
+    #     self.movieRuntime=movieRuntime
+    #     self.movieGenre=movieGenre
+    #     self.movieTagline=movieTagline
+    #     self.movieRating=movieRating
+    #     self.movieUrl=movieUrl
+    #     self.moviePoster=moviePoster
+
+    def to_dict(self):
+        return {"movieTitle": self.movieTitle, "movieDescription": self.movieDescription, "movieKeywords": self.movieKeywords, "movieCast": self.movieCast, "movieDirector": self.movieDirector, "movieProduction": self.movieProduction, "movieRuntime": self.movieRuntime, "movieGenre": self.movieGenre, "movieTagline": self.movieTagline, "movieRating": self.movieRating, "movieUrl": self.movieUrl, "moviePoster": self.moviePoster}
 
 
 class Likes(models.Model):
