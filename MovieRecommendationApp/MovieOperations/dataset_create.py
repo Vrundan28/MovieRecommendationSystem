29503bc9ad7583d928688bfc22c2bf6e9612d6dc
vrundan28 <vrundan1234@gmail.com>
2022-02-17 15:44:17 +0530

    Dataset Updated

--- MovieRecommendationApp/MovieOperations/dataset_create.py
+++ MovieRecommendationApp/MovieOperations/dataset_create.py
@@ -2,6 +2,9 @@ import pandas as pd
 import sqlite3
 import json
 import time
+import imdb
+from imdb import IMDbError
+from csv import writer
 # from MovieOperations.models import Movie
 
 
@@ -16,10 +19,52 @@ import time
 # myconn = sqlite3.connect('..\db.sqlite3')
 # print(chk_conn(myconn))
 
-conn = sqlite3.connect("db.sqlite3")
-cursor = conn.cursor()
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-print(cursor.fetchall())
+# conn = sqlite3.connect("db.sqlite3")
+# cursor = conn.cursor()
+# cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+# print(cursor.fetchall())
+
+dataset = pd.read_csv('tmp_dataset.csv')
+# print (dataset)
+# import imdb
+df = pd.DataFrame(dataset)
+
+feilds = ["movieId", "movieTitle", "movieDescription", "movieKeywords", "movieCast", "movieDirector",
+          "movieProduction", "movieRuntime", "movieGenre", "movieTagline", "movieRating", "movieCover"]
+print(df)
+print("vrundan")
+access = imdb.IMDb()
+rows = []
+err = []
+for i, j in df.iterrows():
+    print(j['movieId'])
+    try:
+        movie = access.search_movie(j['movieTitle'])
+        if(len(movie) > 0):
+            row = [j['movieId'], j['movieTitle'], j['movieDescription'], j['movieKeywords'], j['movieCast'], j['movieDirector'],
+                j['movieProduction'], j['movieRuntime'], j['movieGenre'], j['movieTagline'], j['movieRating'], movie[0]['cover url']]
+        else:
+            row = [j['movieId'], j['movieTitle'], j['movieDescription'], j['movieKeywords'], j['movieCast'], j['movieDirector'],
+               j['movieProduction'], j['movieRuntime'], j['movieGenre'], j['movieTagline'], j['movieRating'], "-"]
+        print(row)
+        rows.append(row)
+    except IMDbError as e:
+        err.append(j['movieId'])
+
+        # print ("title: %s year: %s" % (movie[0]['title'], movie[0]['year']))
+        # print ("Cover url: %s" % movie[0]['cover url'])
+print(rows)
+with open('tmp1_dataset.csv', 'a', encoding="utf-8") as f_object:
+    writer_object = writer(f_object)
+    writer_object.writerow(feilds)
+    writer_object.writerows(rows)
+    f_object.close()
+
+with open('tmp.csv', 'w', encoding="utf-8") as f_object:
+    writer_object = writer(f_object)
+    writer_object.writerow(err)
+    f_object.close()
+
 # cursor = conn.execute("SELECT movieTitle from MovieOperations_movie")
 # for row in cursor:
 #    print ("ID = ", row[0])
